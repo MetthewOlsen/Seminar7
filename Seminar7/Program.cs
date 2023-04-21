c5357caab477253b5567ff8f707007e241cc62e5
MetthewOlsen <metthew_74@mail.ru>
2023-04-21 07:11:21 +0300

    Задача1

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -1,39 +1,47 @@
-﻿// // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+﻿// /*
+// Задача 47. 
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+// */
 
+// int rows = ReadInt("Введите количество строк: ");
+// int colums = ReadInt("Введите количество столбцов: ");
+// double[,] numbers = new double[rows, colums];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
 
-// int[,] CreateRandom2DArray()
+// // Заполнение массива рандомными вещественными числами
+// void FillArray2D(double[,] array)
 // {
-//     Console.Write("Введите количество строк: ");
-//     int rows = Convert.ToInt32(Console.ReadLine());
-//     Console.Write("Введите количество столбцов: ");
-//     int columns = Convert.ToInt32(Console.ReadLine());
-//     Console.Write("Введите минимальное значение: ");
-//     int minValue = Convert.ToInt32(Console.ReadLine());
-//     Console.Write("Введите максимальное значение: ");
-//     int maxValue = Convert.ToInt32(Console.ReadLine());
-
-//     int[,] array = new int[rows, columns];
-
-//     for(int i = 0; i < rows; i++)
-//         for(int j = 0; j < columns; j++)
-//             array[i,j] = new Random().Next(minValue, maxValue+ 1);
-
-//     return array;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(-99, 99) / 10.0;
+//         }
+//     }
 // }
 
-// void Show2DArray(int[,] array)
+// //  Функция вывода массива в терминал
+// void PrintArray2D(double[,] array)
 // {
-//     for(int i = 0; i < array.GetLength(0); i++)
+//     for (int i = 0; i < array.GetLength(0); i++)
 //     {
-//         for(int j = 0; j < array.GetLength(1); j++)
+//         for (int j = 0; j < array.GetLength(1); j++)
 //         {
-//             Console.Write(array[i,j] + " ");
+//             Console.Write(array[i, j] + " ");
 //         }
 //         Console.WriteLine();
 //     }
 //     Console.WriteLine();
 // }
 
-
-// int[,] myArray = CreateRandom2DArray();
-// Show2DArray(myArray);
+// // Функция ввода 
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
