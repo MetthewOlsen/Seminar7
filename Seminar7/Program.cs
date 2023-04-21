b12a0bbad8ad14b0ecdcd1221ebd138c75e36ecc
MetthewOlsen <metthew_74@mail.ru>
2023-04-21 07:04:42 +0300

    Задача1

---
+++ Seminar7/Program.cs
@@ -0,0 +1,39 @@
+﻿// // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+
+// int[,] CreateRandom2DArray()
+// {
+//     Console.Write("Введите количество строк: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите количество столбцов: ");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите минимальное значение: ");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите максимальное значение: ");
+//     int maxValue = Convert.ToInt32(Console.ReadLine());
+
+//     int[,] array = new int[rows, columns];
+
+//     for(int i = 0; i < rows; i++)
+//         for(int j = 0; j < columns; j++)
+//             array[i,j] = new Random().Next(minValue, maxValue+ 1);
+
+//     return array;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// int[,] myArray = CreateRandom2DArray();
+// Show2DArray(myArray);
